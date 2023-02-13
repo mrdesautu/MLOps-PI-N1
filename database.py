6e64ceee6a9bb9a7eb78a0180c4fdd472ba1a851
mrdesautu <109751301+mrdesautu@users.noreply.github.com>
2023-02-13 18:38:39 -0300

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from peewee import *
-import pandas as pd
-netflix= pd.read_csv("netflix_csv")
-hulu=pd.read_csv("hulu_csv")
-disney=pd.read_csv("disney_csv")
-amazon=pd.read_csv("amazon")
-
-netflix= MySQLDatabase("netflix_csv",user="root",host="/home/martin/Escritorio/data engenier/netflix_csv.csv",port=3306)
-
